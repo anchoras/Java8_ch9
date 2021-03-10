ea45eddb6adce2025809d39760652036328667d9
anchoras <anchoras2@gmail.com>
2021-03-10 16:48:36 +0300

    added demo for basics of using THROWABLE

---
+++ ThrowableDemo.java
@@ -0,0 +1,27 @@
+/*
+    demo shows basics of using THROWABLE
+*/
+
+
+class excTest {
+    static void genException() {
+        int nums[] = new int[4];
+        System.out.println("Before generating of exception");
+        nums[7] = 10;
+    }
+}
+
+class ThrowableDemo {
+    public static void main(String args[]) {
+        try {
+            excTest.genException();
+        }
+        catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Standart message: ");
+            System.out.println(exc);
+            System.out.println("\nCall stack: ");
+            exc.printStackTrace();
+        }
+        System.out.println("After CATCH operator");
+    }
+}
