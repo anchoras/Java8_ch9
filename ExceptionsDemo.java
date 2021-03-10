6e8e830c81f57570633ff6fa4a2c836a53a74aaa
anchoras <anchoras2@gmail.com>
2021-03-10 12:36:51 +0300

    modified for demonstration of nested TRY CATCH blocks

--- ExceptionsDemo.java
+++ ExceptionsDemo.java
@@ -7,18 +7,22 @@ class ExceptionsDemo {
         int numsNumer[] = { 4, 8, 16, 32, 64, 128, 512 };
         int numsDenum[] = { 2, 0, 4, 4, 0, 8, 16, 2 };
 
-        for(int i = 0; i<numsDenum.length; i++) {
-            try {
-                System.out.println(numsNumer[i] + "/" + numsDenum[i] +
-                                    " = " + numsNumer[i]/numsDenum[i]);
-            }
+        try {
+            for(int i = 0; i<numsDenum.length; i++) {
+                try {
+                    System.out.println(numsNumer[i] + "/" + numsDenum[i] +
+                                        " = " + numsNumer[i]/numsDenum[i]);
+                }
 
-            catch (ArithmeticException exc) {
-                System.out.println("Dividing by zero");
+                //catch (Throwable exc) {
+                catch (ArithmeticException exc) {
+                    System.out.println("Dividing by zero");
+                }
             }
-            catch (Throwable exc) {
-                System.out.println("Exception was");
-            }
+        }
+        //catch (Throwable exc) {
+        catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Out of array range");
         }
     }
 }
