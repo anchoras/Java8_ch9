9ae969028d3be9e919076386fd51ee6ad0b156a5
anchoras <anchoras2@gmail.com>
2021-03-10 15:17:16 +0300

    modified for showing rethrow exceptions

--- ThrowDemo.java
+++ ThrowDemo.java
@@ -3,22 +3,35 @@
 */
 
 
+class Rethrow {
+    public static void getException() {
+        int numsNumer[] = { 4, 8, 16, 32, 64, 128, 512 };
+        int numsDenum[] = { 2, 0, 4, 4, 0, 8, 16, 2 };
+
+        for(int i=0; i<numsDenum.length; i++) {
+            try {
+                System.out.println(numsNumer[i] + "/" + numsDenum[i] +
+                                    " = " + numsNumer[i]/numsDenum[i]);
+            }
+            catch (ArithmeticException exc) {
+                System.out.println("Dividing by zero");
+            }
+            catch (ArrayIndexOutOfBoundsException exc) {
+                System.out.println("Out of array range");
+                throw exc;
+            }
+        }
+        System.out.println("After CATCH block");
+    }
+}
+
 class ThrowDemo {
     public static void main(String args[]) {
         try {
-            for(int i=0; i<3; i++) {
-                try {
-                    System.out.println("Before THROW operator");
-                    throw new ArithmeticException();
-                }
-                catch (ArithmeticException exc) {
-                    System.out.println("Inner CATCHing arithmetic exception");
-                }
-            }
+            Rethrow.getException();
         }
-        catch (ArithmeticException exc) {
-            System.out.println("Outer CATCHing arithmetic exception");
+        catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("CATHing in main");
         }
-        System.out.println("After CATCH block");
     }
 }
